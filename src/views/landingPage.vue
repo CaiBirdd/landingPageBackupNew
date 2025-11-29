f8c8c9f4079a8eafe640cbd9a2636ebb25e7a40f
CaiBirdd <2960693496@qq.com>
2025-11-29 10:20:08 +0800

    bugfix/- 修复移动端标题换行问题

--- src/views/landingPage.vue
+++ src/views/landingPage.vue
@@ -3,13 +3,13 @@ import Header from '../components/Header.vue';
 import FeedbackForm from '../components/FeedbackForm.vue';
 </script>
 <template>
-  <div class="feedback-container">
+  <div class="feedback-container-pc">
     <Header />
     <FeedbackForm />
   </div>
 </template>
 <style lang="scss" scoped>
-.feedback-container {
+.feedback-container-pc {
   max-width: 1440px;
   margin: 0 auto;
   padding: 60px 0;
@@ -17,7 +17,7 @@ import FeedbackForm from '../components/FeedbackForm.vue';
 }
 
 @media (max-width: 768px) {
-  .feedback-container {
+  .feedback-container-pc {
     padding: 30px 0;
   }
 }
